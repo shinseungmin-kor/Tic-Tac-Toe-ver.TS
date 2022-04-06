41563d49da484c462d5bf586840e4f221e8e8663
신승민 <sinseungmin@sinseungmin-ui-MacBookPro-2.local>
2022-04-06 09:55:08 +0900

    finish

--- src/App.tsx
+++ src/App.tsx
@@ -2,35 +2,41 @@ import React, { useState } from 'react';
 import Board from './Board';
 import './App.css';
 
+interface historyType {
+    squares: string[];
+};
+
 // 타입스트립트에 타입을 굳이 넣지 않는다고 해도, 타입스크립트 자체가 알아서 추론하기때문에 돌아가는데 문제는 없다.
 const App = () => {
-  const [history, setHistory] = useState([{ squares: Array(9).fill(null) }]);
+  const [history, setHistory] = useState <historyType[]>([{ squares: Array(9).fill(null) }]);
   const [stepNumber, setStepNumber] = useState<number>(0);
   const [xIsNext, setXIsNext] = useState<Boolean>(true);
 
-  const handleClick = (n: number) => {
-    const newHistory = history.slice(0, stepNumber + 1);
-    const current = newHistory[newHistory.length - 1];
-    const squares = current.squares.slice();
+  const handleClick = (n: number): void => {
+    const newHistory: historyType[] = [...history];                  // 깊은복사: 전개연산자를 이용한다.
+    // const newHistory = history.slice(0, stepNumber + 1);          // 얕은복사: slice() 를 이용한다.
+    const current: historyType = newHistory[newHistory.length - 1];
+    const squares: string[] = current.squares.slice();
     
     if(calculateWinner(squares) || squares[n]) return;
-    squares[n] = xIsNext ? "X" : "O"
+    squares[n] = xIsNext ? "X" : "O" 
 
-    setHistory(newHistory.concat([{squares: squares,}]));
+    setHistory(newHistory.concat([{squares}]));
     setStepNumber(newHistory.length);
     setXIsNext(!xIsNext)
+    // console.log(current.squares)
   };
 
-  const jumpTo = (step: number) => {
+  const jumpTo = (step: number): void => {
     setStepNumber(step);
     setXIsNext((step % 2) === 0);
   };
 
-  const current = history[stepNumber];
+  const current: historyType = history[stepNumber];
   const winner: string | null = calculateWinner(current.squares);
 
-  const moves = history.map((step, move: number) => {
-    const desc = move ? 'Go to move # ' + move : 'Go to game start';
+  const moves: JSX.Element[] = history.map((step, move: number) => {
+    const desc: string = move ? 'Go to move # ' + move : 'Go to game start';
 
     return (
       <>
@@ -42,6 +48,7 @@ const App = () => {
   });
 
   let status: string;
+
   if(winner) {
     status = 'Winner is: ' + winner;
   } else {
@@ -66,8 +73,8 @@ const App = () => {
   );
 };
 
-const calculateWinner = (squares: string[]) => {      // 승자를 정하는 함수.
-  const lines = [                           // 승리가 정해지는 경우를 가지고 있는 배열
+const calculateWinner = (squares: string[]): string | null => {      // 승자를 정하는 함수.
+  const lines: number[][] = [                           // 승리가 정해지는 경우를 가지고 있는 배열
       [0, 1, 2],
       [3, 4, 5],
       [6, 7, 8],
@@ -78,7 +85,7 @@ const calculateWinner = (squares: string[]) => {      // 승자를 정하는 함
       [2, 4, 6],
   ];
   for (let i = 0; i < lines.length; i++) {  // line 배열을 순환하는 반복문을 만든다
-      const [a, b, c] = lines[i];           // line 의 인덱스 배열을 임의의 배열로 지정하고 조건을 부여한다.
+      const [a, b, c]: number[] = lines[i];           // line 의 인덱스 배열을 임의의 배열로 지정하고 조건을 부여한다.
       if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {  
           return squares[a];
       }
--- src/Board.tsx
+++ src/Board.tsx
@@ -1,39 +1,43 @@
 import React from 'react';
 import Button from './Button';
-import './Board.css'
+import './Board.css';
 
-const Board = ({ square, handleClick }: any) => {
-
-    const renderSquare = (n: number) => {
-        return (
-            <>
-                <Button
-                    vlaue={square[n]}
-                    onCick={() => handleClick(n)}
-                />
-            </>
-        )
-    };
-
-    return (
-        <div>
-            <div className="board-row">
-                {renderSquare(0)}
-                {renderSquare(1)}
-                {renderSquare(2)}
-            </div>
-            <div className="board-row">
-                {renderSquare(3)}
-                {renderSquare(4)}
-                {renderSquare(5)}
-            </div>
-            <div className="board-row">
-                {renderSquare(6)}
-                {renderSquare(7)}
-                {renderSquare(8)}
-            </div>
-        </div>
-    );
+interface IBase {
+  square: any;
 }
+
+interface IProps extends IBase {
+  handleClick: (n: number) => void;
+}
+
+const Board = ({ square, handleClick }: IProps) => {
+  const renderSquare = (n: number) => {
+    return (
+      <>
+        <Button value={square[n]} onClick={() => handleClick(n)} />
+      </>
+    );
+  };
+
+  return (
+    <div>
+      <div className="board-row">
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
+      </div>
+      <div className="board-row">
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
+      </div>
+      <div className="board-row">
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
+      </div>
+    </div>
+  );
+};
 
 export default Board;
